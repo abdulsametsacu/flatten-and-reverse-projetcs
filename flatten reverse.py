bcc3eb5423661e1270d2c59cf374555be2b30ed6
abdulsametsacu <67056122+abdulsametsacu@users.noreply.github.com>
2022-07-15 16:39:46 +0300

    Add files via upload

---
+++ flatten reverse.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[17]:
+
+
+l = [[1,'a',['cat'],2],[[[3]],'dog'],4,5]
+lnew = []
+def flatten(n):
+    for i in n:
+        if isinstance(i, list):
+            flatten(i)
+        else:
+            lnew.append(i)
+
+flatten(l)
+print(lnew)
+
+
+# In[18]:
+
+
+l =[[1, 2], [3, 4], [5, 6, 7]]
+l=l[::-1]
+
+for i in range(len(l)):
+    (l[i])=(l[i])[::-1]
+print(l)
+
+
+# In[ ]:
+
+
+
+
